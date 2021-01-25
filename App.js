dece799adee2da6bf4ca35ea2653716eddd209c0
RsMan <William2000>
2021-01-25 11:27:43 +0100

    nav upgrade

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 import Home from "./components/Home/home";
 import Details from "./components/Home/Details/details";
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faUserAlt} from "@fortawesome/free-solid-svg-icons";
 
 const Stack = createStackNavigator();
 
@@ -12,18 +14,43 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name={"Home"} component={Home}/>
-        <Stack.Screen name={"Details"} component={Details}/>
+        <Stack.Screen name={"Home"} component={Home} options={{
+          title: 'Accueil - Produits' ,
+          headerStyle: styles.headerStyle,
+          headerTintColor: 'white',
+          headerTitle: props => <Nav {...props} />,
+        }}/>
+        <Stack.Screen name={"Details"} component={Details} options={{
+          title: 'Détails' ,
+          headerStyle: styles.headerStyle,
+          headerTintColor: 'white',
+          headerTitle: props => <Nav {...props} />,
+        }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+function Nav(props) {
+  console.log(props);
+  return (
+      <View style={{width:'100%', alignItems:'center', flexDirection: 'row', justifyContent:'space-between', color: props.tintColor, fontFamily:'arial, sans-serif'}}>
+        <View>{props.children}</View>
+        <View style={{fontSize:'20px'}}>LeMauvaisCoin</View>
+        <View><FontAwesomeIcon icon={faUserAlt} color={props.tintColor} size={25}/></View>
+      </View>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+  },
+  headerStyle: {
+    backgroundColor: '#f4511e',
+    borderBottomWidth: '0px',
+    boxShadow: '0 0 7px 0 #BBB'
   },
 });
